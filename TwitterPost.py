d5abc3d9c6aa610a552589e7366920f4f889ae83
brad <bhontz@paiwhq.com>
2019-04-11 09:36:50 -0700

    changes after testing on a fresh RaspPi

--- TwitterPost.py
+++ TwitterPost.py
@@ -2,24 +2,24 @@
    Raspberry Pi Jam GSOC Photo Booth
 
 """
-import json
+import os, json
 from twython import Twython
 
 # might want to change this to @GirlScoutsOC
 # example message:  message = "Thanks for visiting our photobooth!  #WomenInSTEM @GirlScouts"
 
-def TwitterPost(message):
+def TwitterPost(strSaveImagePath, strMessage):
 
     with open('credentials/twitteraccountauthentication.json') as json_file:
         dictKeys = json.load(json_file)
         twitter = Twython(dictKeys["consumer_key"], dictKeys["consumer_secret"], dictKeys["access_token"], dictKeys["access_token_secret"])
         try:
-            image_open = open('picture_overlay.png', 'rb')
+            image_open = open(os.path.join(strSaveImagePath, 'picture_overlay.png'), 'rb')
         except IOError as detail:
             print("** Couldn't find the PICTURE_OVERLAY.PNG file to send! ** %s" % detail)
         else:
             image_id = twitter.upload_media(media=image_open)
-            twitter.update_status(status=message, media_ids=image_id['media_id'])
+            twitter.update_status(status=strMessage, media_ids=image_id['media_id'])
     
         del twitter
     
